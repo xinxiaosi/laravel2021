a536587046ddc7a8e3de7885fc0e6aba6b4364b4
xinxiaosi <1252597511@qq.com>
2021-05-26 19:35:49 +0800

    123

--- app/Http/Controllers/Base/IndexController.php
+++ app/Http/Controllers/Base/IndexController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Repositories\Repository;
 use App\Models\UserCenter\Models\AdminModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Auth;
@@ -15,18 +16,33 @@ use Illuminate\Support\Facades\Auth;
 class IndexController extends Controller
 {
     private $repository;
+
     public function __construct(Repository $repository)
     {
         $this->repository = $repository;
-//        $this->middleware('auth:api', ['except' => ['register', 'login']]);
+//        $this->middleware('auth');
     }
 
     public function index(Request $request)
     {
-        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL3VzZXJDZW50ZXJcL2FkbWluXC9sb2dpbiIsImlhdCI6MTYyMDI5NDc2OSwiZXhwIjoxNjIwMjk4MzY5LCJuYmYiOjE2MjAyOTQ3NjksImp0aSI6InVPbkRGa1hJMUNzczN4aFciLCJzdWIiOjEsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.yYm2EflhBZOMWFCn7lbDJv1qJYEWnFy8EYXjwLoKrWQ";
-        $info = JWTAuth::parseToken()->user();
-        dd($info);
-        return ;
+//        $arr = [1,2,3,4,5,6,7,8,9];
+//        dump((end($arr)));
+//        dd(key(array_slice($arr, -1, 1, true)));
+
+
+        $key = 'zAdd';
+        $redis = Redis::connection('cache');
+
+//        for ($i = 1; $i<100; $i++) {
+//            $redis->zAdd($key, rand(1,100), $i);
+//        }
+
+
+        $a = $redis->zRank($key, 24);
+        dump($a);
+        return ($redis->zRange($key, 99, 555));
+
+        dd(1);
     }
 
     protected function respondWithToken($token)
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -21,32 +21,32 @@ class Authenticate
      */
     public function handle(Request $request, Closure $next)
     {
-//        //获取token
-//        $token = $request->header('token','');
-//        if (empty($token)) {
-//            $token = $request->input('token','');
-//            if(empty($token)){
-//                throw new ApiException(ApiException::ERROR_AUTHORIZATION);
-//            }
-//        }
-//        $token = str_replace(' ','',$token);
-//
-//        //权限验证
-//        env('AUTH') && $this->rulesValidator($token);
-//
-//        //先从缓存 获取 token 的用户信息 如果不存在 再调用 获取用户信息
-//        $info = json_decode(Cache::get(self::CACHE_KEY.$token), true);
-//        if(!$info){
-//            throw new ApiException(ApiException::ERROR_AUTHORIZATION);
-//        } else {
-//            app('config')->set(['userInfo' => $info]);
-//        }
-//
-//        app('config')->set(['pageSize'=> $request->input("pageSize", config('pageSize'))]);
-//        app('config')->set(['token'=> $token]);
-//        $response = $next($request);
-        $response = $next($request);
-        return $response;
+        //获取token
+        $token = $request->header('token','');
+        if (empty($token)) {
+            $token = $request->input('token','');
+            if(empty($token)){
+                throw new ApiException(ApiException::ERROR_AUTHORIZATION);
+            }
+        }
+        $token = str_replace(' ','',$token);
+
+        //权限验证
+        env('AUTH') && $this->rulesValidator($token);
+
+        //先从缓存 获取 token 的用户信息 如果不存在 再调用 获取用户信息
+        $info = json_decode(Cache::get(self::CACHE_KEY.$token), true);
+        if(!$info){
+            throw new ApiException(ApiException::ERROR_AUTHORIZATION);
+        } else {
+            app('config')->set(['userInfo' => $info]);
+        }
+
+        app('config')->set(['pageSize'=> $request->input("pageSize", config('pageSize'))]);
+        app('config')->set(['token'=> $token]);
+
+
+        return $next($request);
     }
 
     /**
